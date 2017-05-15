1d354df53605f2c5f10c6ff41bb8d3fbca5809ca
ShiliangZuo <zuo.shiliang@yahoo.com>
2017-05-14 20:24:23 -0400

    add transient log

--- src/main/java/iiis/systems/os/blockdb/DatabaseEngine.java
+++ src/main/java/iiis/systems/os/blockdb/DatabaseEngine.java
@@ -1,5 +1,9 @@
 package iiis.systems.os.blockdb;
 
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.HashMap;
 
 public class DatabaseEngine {
@@ -15,10 +19,29 @@ public class DatabaseEngine {
 
     private HashMap<String, Integer> balances = new HashMap<>();
     private int logLength = 0;
+    private int id = 1;
     private String dataDir;
+
+    private String printWriterLogPath;
+    //private String bufferedWriterLogPath;
+
+    PrintWriter writer = null;
+    //BufferedWriter bw = null;
+    //FileWriter fw = null;
 
     DatabaseEngine(String dataDir) {
         this.dataDir = dataDir;
+        this.printWriterLogPath = dataDir + "printWriterLog.txt";
+        //this.bufferedWriterLogPath = dataDir + "bufferedWriterLog.txt";
+
+        try{
+            writer = new PrintWriter(printWriterLogPath, "UTF-8");
+            writer.println("Transient Log:");
+            writer.flush();
+            System.out.println("Create and open file successful");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     private int getOrZero(String userId) {
@@ -36,6 +59,13 @@ public class DatabaseEngine {
 
     public boolean put(String userId, int value) {
         logLength++;
+        writer.println("put: " + userId + " " + value);
+        writer.flush();
+
+        if (logLength == 50) {
+            packToJSON();
+        }
+
         System.out.println("logLength" + logLength);
         balances.put(userId, value);
         return true;
@@ -43,6 +73,13 @@ public class DatabaseEngine {
 
     public boolean deposit(String userId, int value) {
         logLength++;
+        writer.println("deposit: " + userId + " " + value);
+        writer.flush();
+
+        if (logLength == 50) {
+            packToJSON();
+        }
+
         int balance = getOrZero(userId);
         balances.put(userId, balance + value);
         return true;
@@ -50,6 +87,13 @@ public class DatabaseEngine {
 
     public boolean withdraw(String userId, int value) {
         logLength++;
+        writer.println("withdraw: " + userId + " " + value);
+        writer.flush();
+
+        if (logLength == 50) {
+            packToJSON();
+        }
+
         int balance = getOrZero(userId);
         if (value > balance) {
             return false;
@@ -60,6 +104,13 @@ public class DatabaseEngine {
 
     public boolean transfer(String fromId, String toId, int value) {
         logLength++;
+        writer.println("transfer: " + fromId + " " + toId + " " + value);
+        writer.flush();
+
+        if (logLength == 50) {
+            packToJSON();
+        }
+
         int fromBalance = getOrZero(fromId);
         int toBalance = getOrZero(toId);
         if (value > fromBalance) {
@@ -72,5 +123,10 @@ public class DatabaseEngine {
 
     public int getLogLength() {
         return logLength;
+    }
+
+    private boolean packToJSON() {
+        
+        return true;
     }
 }
